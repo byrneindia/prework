7cd4301c8a4d9495bb8a7328c4ac5425b46ed8a5
India Byrne <byrneindia@gmail.com>
2020-02-20 14:32:05 -0700

    Added starter index page.

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8" />
-	<title> I have completed the prework!</title>
-	<head>
-		<body>
-<p>I have completed the prework!<p>
-	<body>
+<html lang="en"
+		<head>
+			<meta charset="UTF-8"/>
+			<title> I have completed the prework!</title>
+		</head>
+			<body>
+<p> I have complete the prework! </p>
